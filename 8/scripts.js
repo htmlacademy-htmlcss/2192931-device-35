54a537eb5e0c58868ae3c522f3ebdd8c773f2139
Keks <keks@htmlacademy.ru>
2022-11-08 09:28:27 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/scripts.js
@@ -0,0 +1,9 @@
+const menuBtn = document.querySelector('.menu-btn')
+const menu = document.querySelector('.sub-navigation')
+menuBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  menu.classList.toggle('is-active')
+
+
+
+})
