4ed8cbb4562b3ef5681b9a9f6329a0347f94f59a
Aleksey <111198139+MrTruth34@users.noreply.github.com>
2022-11-11 11:09:41 +0300

    Добавляет правки к последнему блоку

--- scripts.js
+++
@@ -1,9 +0,0 @@
-const menuBtn = document.querySelector('.menu-btn')
-const menu = document.querySelector('.sub-navigation')
-menuBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  menu.classList.toggle('is-active')
-
-
-
-})
